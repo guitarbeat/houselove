54807f37e485b31ddb213ba42561aa6111d34920
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2023-10-23 13:59:30 -0700

    changes?

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import { Route, Routes } from 'react-router-dom'
-import Layout from './components/Layout'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import './App.scss'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import GoogleSheetsProvider from 'react-db-google-sheets';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Resources from './components/Resources';
+import Mediators from './components/Mediators';
+import Contact from './components/Contact';
+import './App.scss';
 
 function App() {
+  const config = {
+    
+    apiKey: process.env.REACT_APP_GOOGLE_SHEETS_API_KEY,
+    spreadsheetId: process.env.REACT_APP_GOOGLE_SHEETS_DOC_ID,
+  };
+
   return (
-    <>
+    <GoogleSheetsProvider config={config}>
       <Routes>
         <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        
-        <Route path="/contact" element={<Contact />} />
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="resources" element={<Resources />} />
+          <Route path="mediators" element={<Mediators />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
-    </>
-  )
+    </GoogleSheetsProvider>
+  );
 }
 
-export default App
+export default App;
