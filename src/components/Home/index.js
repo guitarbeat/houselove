90ff4fc02512b3e83f94087ed9f6d6c19d64cfed
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2023-11-12 01:00:59 +0000

    Update 5 files

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,35 +1,55 @@
-import { useEffect, useState } from 'react';
-import './index.scss';
-import AnimatedLetters from '../AnimatedLetters';
-import backgroundImage from '../../assets/images/background.png';
+import { useEffect, useState } from 'react'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import backgroundImage from '../../assets/images/background.png'
 
 const Home = () => {
-  const [letterClass, setLetterClass] = useState('text-animate');
-  const introductionArray = "Empowering Co-op Communities".split("");
+  const [letterClass, setLetterClass] = useState('text-animate')
+  // Split the introduction text into two lines
+  const line1Array = 'Empowering Cooperative'.split('')
+  const line2Array = 'Communities'.split('')
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
-      setLetterClass('text-animate-hover');
-    }, 4000);
+      setLetterClass('text-animate-hover')
+    }, 4000)
 
     return () => {
-      clearTimeout(timeoutId);
-    };
-  }, []);
+      clearTimeout(timeoutId)
+    }
+  }, [])
 
   return (
     <main className="container home-page">
-      <section className="text-zone" aria-labelledby="main-heading" aria-describedby="main-description">
+      <section
+        className="text-zone"
+        aria-labelledby="main-heading"
+        aria-describedby="main-description"
+      >
+        {/* Split the heading into two lines */}
         <h1 id="main-heading">
-          <AnimatedLetters letterClass={letterClass} strArray={introductionArray} idx={0} />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={line1Array}
+            idx={0}
+          />
+          <br /> {/* Line break to move "Communities" to a new line */}
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={line2Array}
+            idx={15}
+          />
         </h1>
-        <h2 id="main-description">Find conflict mediators, share resources, and build a sustainable community.</h2>
+        <h2 id="main-description">
+          Find conflict mediators, share resources, and build a sustainable
+          community.
+        </h2>
       </section>
       <div className="background-image-container" aria-hidden="true">
         <img src={backgroundImage} alt="" role="presentation" />
       </div>
     </main>
-  );
+  )
 }
 
-export default Home;
+export default Home
